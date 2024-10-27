73bbc4e92a8ceb88a87f956c4abdb8ca49838744
Gamze Şirin <gamze1sirin@gmail.com>
2024-10-28 00:09:32 +0300

    feat: add design app

---
+++ TasarimCalismasi/app/src/main/java/com/example/tasarimcalismasi/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tasarimcalismasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //R kodlama içinde res klasörünü temsil eder.
+        //@ xml içinde res klasörünü temsil eder.
+    }
+}
